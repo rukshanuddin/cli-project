a52bf3c3975247ba3304b3c1463bea4f4129d1f3
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-12-03 12:21:40 -0500

    add pokedex scraping method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,1 +1,18 @@
 require_relative '../config/environment'
+
+class Scrape
+
+    doc = Nokogiri::HTML(open("https://pokemondb.net/pokedex"))
+    pokedex_array = doc.css(".grid-row li")
+
+    def pokedex
+        pokedex_array.each.with_index(1) do |pokedex, idx|
+            version = pokedex.text
+            index = idx
+            info = pokedex.css("href value")
+            url = "https://pokemondb.net#{pokedex.css("a")[0].attributes["href"].value}"
+            poke_vers = Pokedex.new(version, url, info, index)
+        end
+    end
+    
+end

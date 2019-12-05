0db0f756b48db3779b09df80b2f8e85c0f8d30dd
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-12-05 17:44:54 -0500

    cleaned up file to only include pokedex class

--- lib/pokedex.rb
+++ lib/pokedex.rb
@@ -22,23 +22,3 @@ class Pokedex
     end
 
 end
-
-def list_pokedex
-    Pokedex.all.each.with_index(1) do |pokedex, idx|
-        puts "#{idx}. #{pokedex.version}"
-        puts pokedex.url
-        puts pokedex.info
-    end
-end
-
-def welcome(input)
-    Scrape.pokedex
-    if input == "list"
-        list_pokedex
-        puts "Enter your selection:"
-    elsif input == "exit"
-        puts "Gotta Catch 'em All!"
-    else
-        puts "not a valid response"
-    end
-end

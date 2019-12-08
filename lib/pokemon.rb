9fd4115c0d4c6a507b9d92a27f4dac11107f9064
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-12-08 02:02:34 -0500

    comments

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,14 @@
 require_relative '../config/environment'
 
-class Pokemon                                               #Define Pokemon class
-
+class Pokemon                                               
+    #Define Pokemon Class
     attr_accessor :name, :url
     attr_reader :info, :number
+    #Array of all objects
     @@all = []
+    #Array of all Pokemon names
     @@all_names = []
+
     def initialize(name, url)
         @name = name
         @url = url
@@ -13,16 +16,19 @@ class Pokemon                                               #Define Pokemon clas
         @@all << self
         @@all_names << self.name.downcase
     end
+
     def self.all_names
         @@all_names
+    end
+
+    def self.clear_names
+        @@all_names.clear
     end
 
     def number=(number)
         @number = number
     end
-    def self.clear_names
-        @@all_names.clear
-    end
+
     def self.all
         @@all
     end

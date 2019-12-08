6c0d8ad9f3096320ff9dff1032252fe4b1b5a812
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-12-08 02:30:10 -0500

    comments

--- lib/pokedex.rb
+++ lib/pokedex.rb
@@ -5,12 +5,22 @@ class Pokedex
     attr_accessor :version, :url, :info, :index
 
         @@all = []
+
     def initialize(version, url, info, index)
         @version = version
         @url = url
         @index = index
         @info = info
         @@all << self
+    end
+    #define a way to lits all pokedexes
+    def self.list
+        @@all.each.with_index(1) do |pokedex, idx|
+            puts "#{idx}. #{pokedex.version}".red
+            puts pokedex.info.blue
+            puts pokedex.url.green
+            dot_dot_dot(15)
+        end
     end
 
     def self.all
